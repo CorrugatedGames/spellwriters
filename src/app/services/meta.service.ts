30100cc590524064eda184db25c8344171404134
Kyle Kemp <kyle@seiyria.com>
2024-02-20 13:42:39 -0600

    feat(update): electron builds will now see update notifications if they have outdated versions

--- src/app/services/meta.service.ts
+++ src/app/services/meta.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { isInElectron } from '../helpers';
+
+interface VersionInfo {
+  dirty: boolean;
+  raw: string;
+  hash: string;
+  distance: number;
+  tag: string;
+  semver: string;
+  suffix: string;
+  semverString: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class MetaService {
+  private currentVersion = 'v.local';
+  private versionMismatch = false;
+  private newVersion = '';
+
+  private changelogCurrent = '';
+  private changelogAll = '';
+
   private versionInfo = {
     dirty: false,
     raw: 'v.local',
@@ -16,16 +35,15 @@ export class MetaService {
   };
 
   public get versionString() {
-    if (this.versionInfo.distance >= 0 && this.versionInfo.tag) {
-      return `${this.versionInfo.tag} (${this.versionInfo.raw})`;
-    }
+    return this.currentVersion;
+  }
 
-    return (
-      this.versionInfo.tag ||
-      this.versionInfo.semverString ||
-      this.versionInfo.raw ||
-      this.versionInfo.hash
-    );
+  public get hasVersionMismatch() {
+    return this.versionMismatch;
+  }
+
+  public get latestLiveVersion() {
+    return this.newVersion;
   }
 
   constructor() {}
@@ -35,8 +53,63 @@ export class MetaService {
       const response = await fetch('assets/version.json');
       const versionInfo = await response.json();
       this.versionInfo = versionInfo;
+
+      this.currentVersion = this.versionInfoToSemver(versionInfo);
     } catch (e) {
       console.error('Failed to load version info', e);
     }
+
+    try {
+      const changelog = await fetch('assets/CHANGELOG.md');
+      const changelogData = await changelog.text();
+      this.changelogAll = changelogData;
+    } catch {
+      console.error('Could not load changelog (all) - probably on local.');
+    }
+
+    try {
+      const changelog = await fetch('assets/CHANGELOG-current.md');
+      const changelogData = await changelog.text();
+      this.changelogCurrent = changelogData;
+    } catch {
+      console.error('Could not load changelog (current) - probably on local.');
+    }
+
+    this.checkVersionAgainstLiveVersion();
+  }
+
+  private async checkVersionAgainstLiveVersion() {
+    if (!isInElectron()) {
+      return;
+    }
+
+    try {
+      const liveVersionFile = await fetch(
+        'https://play.spellwritersgame.com/assets/version.json',
+      );
+      const liveVersionData = await liveVersionFile.json();
+
+      if (this.versionInfo.hash !== liveVersionData.hash) {
+        this.versionMismatch = true;
+        this.newVersion = this.versionInfoToSemver(liveVersionData);
+      }
+    } catch {
+      console.error(
+        'Could not load live version data. Probably not a big deal.',
+      );
+    }
+  }
+
+  private versionInfoToSemver(versionInfo: VersionInfo) {
+    if (versionInfo.distance >= 0 && versionInfo.tag) {
+      return `${versionInfo.tag} (${versionInfo.raw})`;
+    }
+
+    return (
+      versionInfo.tag ||
+      versionInfo.semverString ||
+      versionInfo.raw ||
+      versionInfo.hash
+    );
   }
 }
